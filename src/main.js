8a28a5fa69c10a157cb464bc549dfc10ae3c2545
SittHmuEain <yoopajdoto@gmail.com>
2022-08-18 15:44:09 +0630

    guard route and intialized vue app for auth

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/global.css'
-createApp(App).use(router).mount('#app')
+import { auth } from './firebase/config';
+let app;
+auth.onAuthStateChanged(()=>{
+    if(!app){
+    createApp(App).use(router).mount('#app')
+    }
+})
